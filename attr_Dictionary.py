f7a665f72b500ac973078ee006bbf4627845987f
Joseph Szokoli <joseph.szokoli@gmail.com>
2018-08-29 18:56:38 -0400

    added separate listFunction
    Separated List function and dictionary build function

--- attr_Dictionary.py
+++ attr_Dictionary.py
@@ -1,121 +1,153 @@
 import maya.cmds as cmds
-def buildAttrDictionary(sourceNode,mode):
-    #Initialize Dictionary
-    attrDict ={}
+
+def listAttrModes(sourceNode,mode):
     listAttrs = []
         
     if mode == "default":
         #List all Attributes
-        listAllAttrs = cmds.listAttr(sourceNode)   
+        listAllAttrs = cmds.listAttr(sourceNode,hd=1)   
         for attr in listAllAttrs:
-            if '.' in attr:
-                pass
-            else:
-                listAttrs.append(attr)
-                
+            listAttrs.append(attr)
+            
         #Remove any user created attrs
         customAttrs = cmds.listAttr(sourceNode,ud=1) or []   
         for attr in customAttrs:
             listAttrs.remove(attr)
-            
-        for attr in listAttrs:
-            connectedAttrs = cmds.listConnections(sourceNode+'.'+attr,c=1)
-            if connectedAttrs == None:
-                pass
-            else:
+            listAllAttrs.remove(attr)
+                  
+        for attr in listAllAttrs:
+            #print attr
+            if '.' in attr:
                 listAttrs.remove(attr)
-       
+                originAttr = attr.split('.')[0]
+                targetAttrs = attr.split('.')[1:]
+                if len(targetAttrs) > 1:                
+                    joiner = '.'
+                    targetAttr = joiner.join(targetAttrs[1:])
+                else:
+                    targetAttr = targetAttrs[0]
+                indices =  cmds.getAttr(sourceNode+'.'+originAttr, multiIndices = True) or ''
+                #print attr,indices, targetAttr
+                for index in indices:
+                    #print sourceNode+'.'+originAttr+'['+str(index)+'].'+targetAttr            
+                    listAttrs.append(originAttr+'['+str(index)+'].'+targetAttr)
+                    connections=cmds.listConnections(sourceNode+'.'+originAttr+'['+str(index)+'].'+targetAttr,s=True,plugs=True)
+                    if connections:
+                        listAttrs.remove(originAttr+'['+str(index)+'].'+targetAttr)
+                    else:
+                        #print originAttr+'['+str(index)+'].'+targetAttr
+                        pass
+                        
+            else:
+                if cmds.getAttr(sourceNode+'.'+attr, type = 1) == 'TdataCompound':
+                    listAttrs.remove(attr)
+                else:
+                    #Non-Compound Attributes
+                    #Check if attribute has incoming connections
+                    connections=cmds.listConnections(sourceNode+'.'+attr,s=True,plugs=True)
+                    #If connections find and store node and attribute
+                    if connections:
+                        #print attr +' HAS INPUTS DAWG'
+                        listAttrs.remove(attr)
+                    #Else query attribute value and store in dictionary
+                    else:
+                        pass
+     
     elif mode == "custom":
         listAttrs = cmds.listAttr(sourceNode,ud=1)
-        
+
     elif mode == "connection":
         #List all Attributes
-        listAllAttrs = cmds.listAttr(sourceNode)   
+        listAllAttrs = cmds.listAttr(sourceNode,hd=1)   
         for attr in listAllAttrs:
-            if '.' in attr:
-                pass
-            else:
-                listAttrs.append(attr)
-        
+            listAttrs.append(attr)
+            
         #Remove any user created attrs
-        customAttrs = cmds.listAttr(sourceNode,ud=1)        
+        customAttrs = cmds.listAttr(sourceNode,ud=1) or []   
         for attr in customAttrs:
             listAttrs.remove(attr)
-            
-        safetyListAttrs = []
-        for attr in listAttrs:
-            safetyListAttrs.append(attr)
-        
-        for attr in safetyListAttrs:
-            connectedAttrs = cmds.listConnections(sourceNode+'.'+attr)
-            if connectedAttrs == None:
+            listAllAttrs.remove(attr)
+                  
+        for attr in listAllAttrs:
+            #print attr
+            if '.' in attr:
                 listAttrs.remove(attr)
+                originAttr = attr.split('.')[0]
+                targetAttrs = attr.split('.')[1:]
+                if len(targetAttrs) > 1:                
+                    joiner = '.'
+                    targetAttr = joiner.join(targetAttrs[1:])
+                else:
+                    targetAttr = targetAttrs[0]
+                indices =  cmds.getAttr(sourceNode+'.'+originAttr, multiIndices = True) or ''
+                #print attr,indices, targetAttr
+                for index in indices:
+                    #print sourceNode+'.'+originAttr+'['+str(index)+'].'+targetAttr            
+                    listAttrs.append(originAttr+'['+str(index)+'].'+targetAttr)
+                    connections=cmds.listConnections(sourceNode+'.'+originAttr+'['+str(index)+'].'+targetAttr,s=True,plugs=True)
+                    #print cmds.listConnections(sourceNode+'.'+originAttr+'['+str(index)+'].'+targetAttr,s=True,plugs=True)
+                    if connections:
+                        pass
+                    else:
+                        #print originAttr+'['+str(index)+'].'+targetAttr
+                        listAttrs.remove(originAttr+'['+str(index)+'].'+targetAttr)
             else:
-                pass
-    #Build Dictionary
+                if cmds.getAttr(sourceNode+'.'+attr, type = 1) == 'TdataCompound':
+                    listAttrs.remove(attr)
+                else:
+                    #Non-Compound Attributes
+                    #Check if attribute has incoming connections
+                    connections=cmds.listConnections(sourceNode+'.'+attr,s=True,plugs=True)
+                    #If connections find and store node and attribute
+                    if connections:
+                        print attr +' HAS INPUTS DAWG'
+                        pass
+                    #Else query attribute value and store in dictionary
+                    else:
+                        listAttrs.remove(attr)
+
+    return listAttrs
+
+
+def buildAttrDictionary(sourceNode,mode):
+    #Initialize Dictionary
+    attrDict ={}
+    listAttrs = listAttrModes(sourceNode,mode) 
+                    
+    #Build Dictionary   
     for attr in listAttrs:
         #If a Compound Attribute
-        #if cmds.attributeQuery(attr,node=sourceNode,at=1) == 'compound':
-        if cmds.getAttr(sourceNode+'.'+attr,type=1) == 'TdataCompound':
-            #print attr
-            #Find Children Attr names
-            compoundChildren = cmds.attributeQuery(attr,node=sourceNode,lc=1) or [attr]
-            
-            #Find Children index 
-            numberChildren = cmds.getAttr(sourceNode+'.'+attr,mi=1)
-            #print attr, compoundChildren, numberChildren
-            for child in compoundChildren:
-                #print attr, compoundChildren, numberChildren
-                if numberChildren == None:
-                    pass
-                else:
-                    for x in numberChildren:
-                        connections=cmds.listConnections(sourceNode+'.'+attr+'['+str(x)+'].'+child,s=True,plugs=True)
-                        if connections:
-                            attrVal=connections
-                            attrDict[attr+'['+str(x)+'].'+child] = attrVal
-                        else:
-                            #Query value of attribute and store in dictionary 
-                            attrVal = cmds.getAttr(sourceNode+'.'+attr+'['+str(x)+'].'+child)
-                            attrDict[attr+'['+str(x)+'].'+child] = attrVal
-               
-                                        
+        #Non-Compound Attributes
+        
+        #Check if attribute has incoming connections
+        connections=cmds.listConnections(sourceNode+'.'+attr,s=True,plugs=True)
+        #If connections find and store node and attribute
+        if connections:
+            attrVal=connections
+            attrDict[attr] = attrVal
+        #Else query attribute value and store in dictionary
         else:
-            #Non-Compound Attributes
-            #Check if attribute has incoming connections
-            connections=cmds.listConnections(sourceNode+'.'+attr,s=True,plugs=True)
-            #If connections find and store node and attribute
-            if connections:
-                attrVal=connections
-                attrDict[attr] = attrVal
-            #Else query attribute value and store in dictionary
-            else:
-                try:
-                    attrVal = cmds.getAttr(sourceNode +'.' +attr)
-                    attrDict[attr] = attrVal
-                except:
-                    pass
-                    #print attr, 'Val was none'
-                
+            attrVal = cmds.getAttr(sourceNode +'.' +attr)
+            attrDict[attr] = attrVal 
+
     #Return Nodes Attribute dictionary
     return attrDict
 
 
-
-
-
 def restoreNodeFromDict(targetNode,attrDict):
+    attrTypeList = []
     for attr,value in attrDict.iteritems():
-        if '.' in attr:
-            attrType = 'TdataCompound'
-        else:
-            if cmds.attributeQuery(attr,node=targetNode,exists=1) == True:
-                attrType = cmds.getAttr(targetNode+'.'+attr,type=1)
-            else:
-                attrType = 'None'
-
+        
+        attrType = cmds.getAttr(targetNode+'.'+attr,type=1)
+        if attrType not in attrTypeList:
+            attrTypeList.append(attrType)
+        
         if attrType == 'float':
             cmds.setAttr(targetNode+'.'+attr,value)
+            
+        elif attrType == 'float2':
+            value = value[0]
+            cmds.setAttr(targetNode+'.'+attr,value[0],value[1],type="float2")
         
         elif attrType == 'float3':
             value = value[0]
@@ -123,7 +155,7 @@ def restoreNodeFromDict(targetNode,attrDict):
         
         elif attrType == 'byte':
             cmds.setAttr(targetNode+'.'+attr,value)
-        
+
         elif attrType == 'bool':
             cmds.setAttr(targetNode+'.'+attr,value)
              
@@ -134,20 +166,25 @@ def restoreNodeFromDict(targetNode,attrDict):
                 cmds.setAttr(targetNode+'.'+attr,'', type="string")
                  
         elif attrType == 'TdataCompound':
-            print attr
-            if isinstance(value, list) == True:
-                value = value[0]
-                cmds.setAttr(targetNode+'.'+attr,value[0],value[1],value[2],type="double3")
-            else:
-                cmds.setAttr(targetNode+'.'+attr,value)
-                
+            pass
+            
+        elif attrType == 'enum':
+            cmds.setAttr(targetNode+'.'+attr,value)
+            
+        elif attrType == 'string':
+            if value == None:
+                value = ''
+            cmds.setAttr(targetNode+'.'+attr,value, type="string")            
+    #print attrTypeList                
 
-
-
-
-
-attrDict = buildAttrDictionary('areaLightShape1',"default")
+attrDict = buildAttrDictionary('ref_cool_RAMP2',"default")
 print attrDict
 
-#restoreNodeFromDict('ramp1',attrDict)
+#attrList = listAttrModes('ref_cool_RAMP2',"default")
+#print attrList
+
+restoreNodeFromDict('ref_cool_RAMP2',attrDict)
+
+
+
 
